14f53c219d8c6ae76fba290eb3f447bdf0b4803f
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-07-11 17:10:46 -0400

    not sure

--- functions.php
+++ functions.php
@@ -1,14 +1,19 @@
 <?php
 include_once(__DIR__ . '/includes/register_assets.php');
-// Set up Navbar
+// Modify Navigation
 function add_classes_on_li($classes, $item, $args)
 {
     $classes[] = 'nav-item';
     return $classes;
 }
-
-
+function add_classes_on_a($atts)
+{
+    $atts['class'] = 'nav-link';
+    return $atts;
+}
+// Add filters to Modify Navigation
 add_filter('nav_menu_css_class', 'add_classes_on_li', 1, 3);
+add_filter('nav_menu_link_attributes', 'add_classes_on_a');
 
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
