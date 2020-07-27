6562d5178c351fb21e4fd4633e6e5735a8f59a60
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-07-27 15:02:43 -0400

    added comment as reminder

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
+    <!-- FUCKING VIEW PORT IMPORTANT -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
